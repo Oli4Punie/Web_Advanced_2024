18b44ec5b839b1346080b9d240383d9c551ebb95
Oli4Punie <11801972@student.pxl.be>
2024-02-26 14:25:58 +0100

    jnhjj

---
+++ Javascript/Functies/Functies.js
@@ -0,0 +1,15 @@
+let telOp = function (getal1, getal2) {
+    return getal1 + getal2
+}
+
+console.log((function (a,b) {
+    return a/b;
+})(6,2))
+
+console.log(
+    telOp(10,20)
+)
+console.log(vermenigvuldig(2,5))
+function vermenigvuldig(a, b) {
+    return a * b;
+}
---
+++ Javascript/Objecten/intro.js
@@ -0,0 +1,9 @@
+let auto = {
+    merk: 'mercedes',
+    bouwjaar: 2024,
+    kleur: 'rood',
+    tweedehands: false,
+    props: {}
+}
+
+const producten = []
